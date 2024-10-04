13fa17c108400d9365a31c3a33122fec66e7190e
Kamal Hashi <kamalweheliye@gmail.com>
2024-10-04 12:40:05 +0400

    add contains_duplicate_217 leetcode

---
+++ tests/test_contains_duplicate_0217.py
@@ -0,0 +1,15 @@
+import pytest
+from leet_code_python.contains_duplicate_0217 import Solution
+
+@pytest.mark.parametrize("nums, expected", [
+    ([1, 2, 3, 1], True),
+    ([1, 2, 3, 4], False),
+    ([1, 1, 1, 1], True),
+    ([], False),
+    ([10], False)
+])
+
+def test_contains_duplicate(nums, expected):
+    sol = Solution()
+    assert sol.contains_duplicate(nums) == expected
+    
